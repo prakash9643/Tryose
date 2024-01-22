7910460d3184bcdbd4446a80468c0b16d50e76b3
Prakash Jha <prakashjha241@gmail.com>
2024-01-22 21:21:02 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ShopNowSection from './Components/Header/Banner_2';
 import NewSectionfooter from './Components/Footer/Footer_newpart';
 import Customer from './Components/Header/Customer';
 import Product_home from './Components/Page/Product_home';
+import Product_detail from './Components/Page/Product_detail';
+import Cart from './Components/Page/cart-page';
+import CartList from './Components/Page/cart-list-page';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -17,6 +20,9 @@ function App() {
       <ShopNowSection />
       <Customer />
       <Product_home />
+      <Product_detail />
+      <Cart />
+      <CartList />
       <NewSectionfooter />
       <Footer />
     </Router>
